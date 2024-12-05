f99ca74fc7c2d84d067cc773e2fad445b1a4469a
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-05 12:33:06 -0300

    Cria serviço que consulta os repositórios no github

--- api/src/main/java/com/kontulari/desafio_tecnico/service/ProfileGithubService.java
+++ api/src/main/java/com/kontulari/desafio_tecnico/service/ProfileGithubService.java
@@ -1,11 +1,14 @@
 package com.kontulari.desafio_tecnico.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kontulari.desafio_tecnico.client.GithubClient;
 import com.kontulari.desafio_tecnico.entity.GithubProfile;
+import com.kontulari.desafio_tecnico.entity.GithubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProfileGithubService {
@@ -16,10 +19,19 @@ public class ProfileGithubService {
     @Value("${github.api.url}")
     private String BASE_URL;
 
-    public GithubProfile getProfile(String username){
+    public GithubProfile getProfile(String username) {
         String endpoint = BASE_URL + "users/" + username;
         GithubProfile profile = githubClient.getClient().getForObject(endpoint, GithubProfile.class);
 
         return profile;
+    }
+
+    public GithubRepository[] getRepositories(String username) {
+        String endpoint = BASE_URL + "users/" + username + "/repos";
+
+        ResponseEntity<GithubRepository[]> response = this.githubClient.getClient().getForEntity(endpoint, GithubRepository[].class);
+        GithubRepository[] repositories = response.getBody();
+
+        return repositories;
     }
 }
