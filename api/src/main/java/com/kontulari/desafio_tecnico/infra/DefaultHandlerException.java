6762d20df10bb0a6410a58ca7a48208fc3581561
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-05 17:15:45 -0300

    Cria corpo de erro para padronizar o resultado das exceções

---
+++ api/src/main/java/com/kontulari/desafio_tecnico/infra/DefaultHandlerException.java
@@ -0,0 +1,12 @@
+package com.kontulari.desafio_tecnico.infra;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class DefaultHandlerException {
+    String message;
+}
