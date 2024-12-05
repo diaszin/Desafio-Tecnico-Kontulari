563ccd73092a02983741d9ab0614881dae57060b
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-05 00:21:24 -0300

    Inicializa backend

---
+++ api/src/test/java/com/kontulari/desafio_tecnico/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kontulari.desafio_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
