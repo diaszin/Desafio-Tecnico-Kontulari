fda7592c39323cdafde7aeeae8e0ec33e8882529
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-08 16:24:51 -0300

    Configura CORS da aplicação

---
+++ api/src/main/java/com/kontulari/desafio_tecnico/configuration/CORSConfiguration.java
@@ -0,0 +1,20 @@
+package com.kontulari.desafio_tecnico.configuration;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String ACCEPTED_URL_CONNECTION = System.getenv().get("ACCEPTED_URL_CONNECTION");
+        registry.addMapping("/**")
+                .allowedOrigins(ACCEPTED_URL_CONNECTION)
+                .allowedMethods("*");
+    }
+}
