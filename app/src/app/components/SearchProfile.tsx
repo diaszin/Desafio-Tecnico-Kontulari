2763bb74c7825fad89dfa3173aac880d62904648
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-09 12:38:20 -0300

    Melhora responsividade do elemento

--- app/src/app/components/SearchProfile.tsx
+++ app/src/app/components/SearchProfile.tsx
@@ -16,16 +16,16 @@ export default function SearchProfile(props: SearchProfileProps) {
     }
     
    }}>
-    <div className='w-full flex items-center justify-between h-[3rem] gap-4'>
+    <div className='w-full flex items-center justify-between h-[3rem] gap-4 max-lg:flex-col max-lg:gap-1'>
       <input onChange={(ev)=>{
         const typedUsername = ev.currentTarget.value
         if(typedUsername.trim().length > 0){
           setUsername(typedUsername)
         }
       }} placeholder='Digite o nome do usuário' autoFocus
-      className='w-[32rem] px-4 outline-none h-full rounded-md font-inter text-[1.25rem] border-2 border-[#D9D9D9] placeholder:font-thin placeholder:text-white bg-transparent' 
+      className='w-[32rem] bg-transparent px-4 outline-none h-full rounded-md font-inter text-[1.25em] border-2 border-[#D9D9D9] placeholder:font-thin placeholder:text-white max-lg:w-full' 
       type="text" id="search-profile" />
-      <button className='font-inter font-semibold rounded-md bg-[#3C3C3C] text-white h-full w-[9rem] transition-colors hover:bg-kontulari-color hover:opacity-85'>Buscar</button>
+      <button className='font-inter font-semibold rounded-md bg-[#3C3C3C] text-white h-full w-[9rem] transition-colors lg:hover:bg-kontulari-color lg:hover:opacity-85 max-lg:w-full'>Buscar</button>
     </div>
    </form>
   )
