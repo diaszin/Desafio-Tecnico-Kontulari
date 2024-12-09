51e70185dd553a647b86a481a3764df2e9ac939c
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-09 12:36:15 -0300

    Altera mensagem alternativa do erro

--- app/src/app/components/SimpleErrorText.tsx
+++ app/src/app/components/SimpleErrorText.tsx
@@ -5,7 +5,7 @@ interface SimpleErrorTextProps{
 export default function SimpleErrorText(props: SimpleErrorTextProps) {
   return (
     <div className='flex items-center justify-center'>
-      <span className='text-gray-300'>{props.message || "Não encontramos nenhum repositório"}</span>
+      <span className='text-gray-300'>{props.message || "Não foi possível consultar o perfil"}</span>
     </div>
   )
 }
