8c8fd67b33afb1f44107630b2a7b221988cc2ca8
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-08 22:44:25 -0300

    Cria componente de Loading

---
+++ app/src/app/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './index.module.css'
+
+/** 
+ * @description Esse componente foi criado pela a página: https://uiverse.io/Shoh2008/nervous-lizard-11
+*/
+export default function Loading() {
+  return (
+    <div className={style['loader']}></div>
+  )
+}
