6b6385c80db17f39f795192cc371b5547aa924a9
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-09 12:29:56 -0300

    Cria metódo que consulta os repositórios do usuário

--- app/src/app/services/Profile.ts
+++ app/src/app/services/Profile.ts
@@ -2,11 +2,18 @@ import axios from "axios"
 import GithubProfile from "../types/GithubProfile"
 
 export default class Profile{
+    
     private BASE_URL: string = process.env.NEXT_PUBLIC_BACKEND_URL + "/profile"
 
     async getGithubUser(username: string): Promise<GithubProfile>{
         const endpoint = `${this.BASE_URL}/${username}/user`
         const response = await axios.get(endpoint)
         return response.data
+    }
+
+    async getGithubRepositoriesByUser(username: string) {
+      const endpoint = `${this.BASE_URL}/${username}/repositories`
+      const response = await axios.get(endpoint)
+      return response.data
     }
 }
