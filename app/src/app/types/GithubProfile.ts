67cd28c7ee9bfece17283a5b29735b20fe408fa1
diaszin <84644190+diaszin@users.noreply.github.com>
2024-12-08 22:37:35 -0300

    Cria interface para o perfildo github

---
+++ app/src/app/types/GithubProfile.ts
@@ -0,0 +1,9 @@
+export default interface GithubProfile{
+    login: string
+    created_at: string
+    html_url: string
+    followers: number
+    photo_url: string
+    public_repos: number
+    description: string
+}
